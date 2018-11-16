6442a3e63639d2b7343f83757b93cfe735ad573d
zhongyuan <zhongyuan.nlp@gmail.com>
2018-11-16 08:38:18 +0800

    define TreeNode

--- Tree.h
+++ Tree.h
@@ -4,7 +4,15 @@
 #define ZY_TREE_H
 
 namespace zy {
+    template<typename V>
+    class TreeNode {
+    public:
+        V val_;
 
+        explicit TreeNode(V v) :val_(v) {}
+
+        V val() const { return val_; }
+    };
 }
 
 #endif
